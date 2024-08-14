b535bce1037a4c3b64d9d7362207da6092771034
Syed Ali Rizwan <alirizwan2003@gmail.com>
2024-08-14 21:26:14 +0500

    utils defined

--- src/index.js
+++ src/index.js
@@ -1,14 +1,27 @@
+// entry point
+
 import dotenv from "dotenv";
 import connectDB from "./db/index.js";
+import { app } from "./app.js";
 
 dotenv.config({
-    path: './env'
-})
+    path: "./env",
+});
 
-connectDB();
+connectDB()
+    .then(() => {
+        app.on("error", (error) => {
+            console.log("MongoDB Connection ERROR: ", error);
+            throw error;
+        });
 
-
-
+        app.listen(process.env.PORT || 8000, () => {
+            console.log(`Server listening on port: ${process.env.PORT}`);
+        });
+    })
+    .catch((error) => {
+        console.log("MongoDB connection error:", error);
+    });
 
 /* 
 import express from "express";
